f6b5d10caa5564fc53cd534ed016b5d39e95a517
Nlyfdnc <115823900+Nlyfdnc@users.noreply.github.com>
2024-12-22 17:28:06 +0300

    Su Tüketim Soruları

--- ygkdemo/lib/main.dart
+++ ygkdemo/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ygkdemo/Admin/admin_login.dart';
 
+import 'package:ygkdemo/Pages/iklimquiz.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -32,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: AdminLogin(),
+      home: IklimQuestion(),
     );
   }
 }
